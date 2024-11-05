7f2b59cc8622e824f767246a4ece82ca118c0f55
liorkasti <liorkasti@gmail.com>
2024-11-05 14:59:54 +0200

    add profile screen requirements

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,16 +1,67 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {FC} from 'react';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
 
-type Props = {};
+import {signOutStr, totalExpensesItemsStr} from '../constants';
+import {COLORS} from '../constants/theme';
+import {removeUser} from '../redux/slices/user-slice';
+import {RootStateType, ScreenProps} from '../redux/types';
 
-const ProfileScreen = (props: Props) => {
+type ProfileScreenProps = ScreenProps<'ProfileScreen'>;
+
+const ProfileScreen: FC<ProfileScreenProps> = ({navigation}) => {
+  const dispatch = useDispatch();
+
+  const {expenses} = useSelector((state: RootStateType) => state.expenses);
+
+  const handleSignout = () => {
+    dispatch(removeUser());
+    navigation.navigate('WelcomeScreen');
+  };
   return (
-    <View>
-      <Text>ProfileScreen</Text>
+    <View style={styles.container}>
+      <View style={styles.totalLine}>
+        <Text style={styles.text}>{totalExpensesItemsStr}</Text>
+        <Text style={styles.total}>{expenses.length}</Text>
+      </View>
+      <View style={styles.separator} />
+      <TouchableOpacity onPress={handleSignout}>
+        <Text style={styles.text}>{signOutStr}</Text>
+      </TouchableOpacity>
+      <View style={styles.separator} />
     </View>
   );
 };
 
-export default ProfileScreen;
+const styles = StyleSheet.create({
+  //TODO: add custom styles in theme.tsx
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 24,
+    backgroundColor: COLORS.bkg,
+  },
+  totalLine: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  text: {
+    paddingVertical: 30,
+    fontWeight: '400',
+    fontSize: 17,
+    color: COLORS.title,
+  },
+  total: {
+    paddingVertical: 30,
+    fontWeight: '700',
+    fontSize: 17,
+    color: COLORS.title,
+  },
+  separator: {
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    marginTop: -20,
+  },
+});
 
-const styles = StyleSheet.create({});
+export default ProfileScreen;
